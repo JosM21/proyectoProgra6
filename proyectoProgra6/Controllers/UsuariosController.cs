d504152bbb8d4654eec8a1e1a4e0b9e2215ab1c1
JOSELIN MARIA CAMPOS VALVERDE <208100383@castrocarazo.ac.cr>
2023-08-19 23:47:05 -0600

    19

--- proyectoProgra6/Controllers/UsuariosController.cs
+++ proyectoProgra6/Controllers/UsuariosController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoProgra6.Attributes;
 using proyectoProgra6.Models;
+using proyectoProgra6.ModelsDTOs;
 
 namespace proyectoProgra6.Controllers
 {
@@ -64,6 +65,64 @@ namespace proyectoProgra6.Controllers
 
             return usuario;
         }
+
+
+        [HttpGet("GetUserInfoByEmail")]
+        public ActionResult<IEnumerable<UsuarioDTO>> GetUserInfoByEmail(string Pemail)
+        {
+            //acá creamos un linq que combina información de dos entidades 
+            //(user inner join userrole) y la agreaga en el objeto dto de usuario 
+
+            var query = (from u in _context.Usuarios
+                         join ur in _context.UsuarioRols on
+                         u.FkUsuarioRol equals ur.IdUsuarioRol
+                         where u.Email == Pemail && u.Active == true &&
+                         u.IsBlocked == false
+                         select new
+                         {
+                             iduser = u.IdUsuario,
+                             email = u.Email,
+                             password = u.Contrasenia,
+                             name = u.Nombre,
+                             backupemail = u.BackUpEmail,
+                             phone = u.Telefono,
+                             address = u.Direccion,
+                             active = u.Active,
+                             isBlocked = u.IsBlocked,
+                             idrol = ur.IdUsuarioRol,
+                             descripcionrol = ur.Descripcion
+                         }).ToList();
+
+
+            //creamos un objeto del tipo que retorna la función 
+            List<UsuarioDTO> list = new List<UsuarioDTO>();
+
+            foreach (var item in query)
+            {
+                UsuarioDTO NewItem = new UsuarioDTO()
+                {
+                    IDUser = item.iduser,
+                    Email = item.email,
+                    Password = item.password,
+                    Name = item.name,
+                    BackupEmail = item.backupemail,
+                    Phone = item.phone,
+                    Addres = item.address,
+                    Activa = item.active,
+                    ItsBlocked = item.isBlocked,
+                    IDRol = item.idrol,
+                    DescriptionRol = item.descripcionrol
+                };
+
+                list.Add(NewItem);
+            }
+
+            if (list == null) { return NotFound(); }
+
+            return list;
+
+        }
+
 
         // PUT: api/Usuarios/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
---
+++ proyectoProgra6/ModelsDTOs/UsuarioDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace proyectoProgra6.ModelsDTOs
+{
+    public class UsuarioDTO
+    {
+
+        //a modo de ejemplo los atributos del modelo estarán en INGLES
+
+        //un escenario como este puede servir para que un equipo de trabajo del app
+        //que solo sepa español, entienda de qué trata 
+
+        public int IDUser { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string BackupEmail { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? Addres { get; set; }
+        public bool? Activa { get; set; }
+        public bool? ItsBlocked { get; set; }
+        public int IDRol { get; set; }
+        public string DescriptionRol { get; set; } = null!;
+
+    }
+}
+
